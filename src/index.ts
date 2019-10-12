e449d3c90f4f6baacc914f8ff4b71386ae16238f
Louis Quinnell <zioroboco@gmail.com>
2019-10-12 20:32:20 +1100

    Clean up template boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application } from "probot"
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+  app.on("issues.opened", async context => {
+    const issueComment = context.issue({
+      body: ":moyai:",
+    })
     await context.github.issues.createComment(issueComment)
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,52 +1,38 @@
-// You can import your modules
-// import index from '../src/index'
+import nock from "nock"
+import moaiMerge from "../src"
+import { Probot } from "probot"
+import payload from "./fixtures/issues.opened.json"
 
-import nock from 'nock'
-// Requiring our app implementation
-import myProbotApp from '../src'
-import { Probot } from 'probot'
-// Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+const issueCreatedBody = { body: ":moyai:" }
 
 nock.disableNetConnect()
 
-describe('My Probot app', () => {
+describe("moai-merge", () => {
   let probot: any
 
   beforeEach(() => {
-    probot = new Probot({ id: 123, cert: 'test' })
-    // Load our app into probot
-    const app = probot.load(myProbotApp)
+    probot = new Probot({ id: 123, cert: "test" })
+    const app = probot.load(moaiMerge)
 
     // just return a test token
-    app.app = () => 'test'
+    app.app = () => "test"
   })
 
-  test('creates a comment when an issue is opened', async (done) => {
+  test("creates a comment when an issue is opened", async done => {
     // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+    nock("https://api.github.com")
+      .post("/app/installations/2/access_tokens")
+      .reply(200, { token: "test" })
 
     // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
+    nock("https://api.github.com")
+      .post("/repos/zioroboco/moai-merge/issues/1/comments", (body: any) => {
         done(expect(body).toMatchObject(issueCreatedBody))
         return true
       })
       .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive({ name: "issues", payload })
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about using TypeScript in your tests, Jest recommends:
-// https://github.com/kulshekhar/ts-jest
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
