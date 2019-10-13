7a681641160dfb622c19138fe2f180cffa361d66
Louis Quinnell <zioroboco@gmail.com>
2019-10-13 19:23:30 +1100

    Update status if PR title is conventional

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,21 @@ import { parse } from "./parser"
 
 export type PullRequestContext = Context<WebhookPayloadPullRequest>
 
-const getLastCommit = async (context: PullRequestContext) => {
+const parseMessage = async (context: PullRequestContext) => {
   const { data } = await context.github.pulls.listCommits(
     context.repo({ pull_number: context.payload.pull_request.number })
   )
-  return parse(data[0].commit.message)
+  return data.length > 1
+    ? parse(context.payload.pull_request.title)
+    : parse(data[0].commit.message)
 }
 
 export const updateStatus = async (context: PullRequestContext) => {
-  const lastCommit = await getLastCommit(context)
+  const { conventional } = await parseMessage(context)
   await context.github.repos.createStatus(
     context.repo({
       sha: context.payload.pull_request.head.sha,
-      state: lastCommit.conventional ? "success" : "failure",
+      state: conventional ? "success" : "pending",
       context: "moai-merge",
     })
   )
--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,35 +1,96 @@
 import Octokit from "@octokit/rest"
 import nock from "nock"
 import { GitHubAPI } from "probot/lib/github"
-import { DeepPartial } from "ts-essentials"
 import { PullRequestContext, updateStatus } from "../src/app"
 
 nock.disableNetConnect()
 
-const context: DeepPartial<PullRequestContext> = {
+const HEAD_SHA = "c0ffee"
+
+const makeContext = (title: string): PullRequestContext => ({
   repo: params => ({ owner: "zioroboco", repo: "moai-merge", ...params }),
   github: new Octokit() as GitHubAPI,
   payload: {
     pull_request: {
       number: 1,
-      title: "More coffee",
+      title,
+      // @ts-ignore
       head: {
-        sha: "c0ffee",
+        sha: HEAD_SHA,
       },
     },
   },
-}
+})
 
-it("updates status", async () => {
-  const scope = nock("https://api.github.com")
+const makeScope = (commits: string[], expected: "success" | "pending") =>
+  nock("https://api.github.com")
     .get("/repos/zioroboco/moai-merge/pulls/1/commits")
-    .reply(200, [{ commit: { message: "fix: address coffee shortage" } }])
-    .post("/repos/zioroboco/moai-merge/statuses/c0ffee", {
-      state: "success",
+    .reply(200, commits.map(message => ({ commit: { message } })))
+    .post(`/repos/zioroboco/moai-merge/statuses/${HEAD_SHA}`, {
+      state: expected,
       context: "moai-merge",
     })
     .reply(200)
 
-  await updateStatus(context as PullRequestContext)
-  expect(scope.isDone()).toBe(true)
+const setup = (params: {
+  title: string
+  commits: string[]
+  expected: "success" | "pending"
+}) => ({
+  context: makeContext(params.title),
+  scope: makeScope(params.commits, params.expected),
+})
+
+describe("a single commit", () => {
+  const title = "title"
+  const commits = ["feat: one"]
+
+  describe("when conventional", () => {
+    it("resolves to success", async () => {
+      const expected = "success"
+
+      const { context, scope } = setup({ title, commits, expected })
+      await updateStatus(context)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+
+  describe("when non-conventional", () => {
+    it("resolves to pending", async () => {
+      const commits = ["one"]
+      const expected = "pending"
+
+      const { context, scope } = setup({ title, commits, expected })
+      await updateStatus(context)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+})
+
+describe("multiple commits", () => {
+  const commits = ["one", "two"]
+
+  describe("with a conventional PR title", () => {
+    const title = "feat: title"
+
+    it("resolves to success", async () => {
+      const expected = "success"
+
+      const { context, scope } = setup({ title, commits, expected })
+      await updateStatus(context)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
+
+  describe("without a conventional PR title", () => {
+    const title = "title"
+
+    it("resolves to pending", async () => {
+      const expected = "pending"
+
+      const { context, scope } = setup({ title, commits, expected })
+      await updateStatus(context)
+      expect(scope.isDone()).toBe(true)
+    })
+  })
 })
